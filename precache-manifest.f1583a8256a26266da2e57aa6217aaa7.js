ff701e4d86f61f18abab082524b608124f92bc84
Agnieszka Wypich <agne6ka@gmail.com>
2020-01-25 10:07:15 +0100

    Updates

---
+++ precache-manifest.f1583a8256a26266da2e57aa6217aaa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bddd78651ed8f28d368172221f060ea",
+    "url": "/my-bookshelf/index.html"
+  },
+  {
+    "revision": "08e782eb6d9bcffc67b8",
+    "url": "/my-bookshelf/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "64c3cbb274946e97643f",
+    "url": "/my-bookshelf/static/js/2.d8dcbe7b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/my-bookshelf/static/js/2.d8dcbe7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08e782eb6d9bcffc67b8",
+    "url": "/my-bookshelf/static/js/main.2aecd31d.chunk.js"
+  },
+  {
+    "revision": "8804a9a94536dfe8e203",
+    "url": "/my-bookshelf/static/js/runtime-main.799e26c9.js"
+  }
+]);
